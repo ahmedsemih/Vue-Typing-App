b10df16aa7a693a3538416de81a7dd35f0d9ead0
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2024-02-15 09:02:02 +0300

    feat: create typing view

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import TypingView from '../views/TypingView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    { path: '/', component: TypingView }
+  ]
 })
 
 export default router;
---
+++ src/views/TypingView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import TopBar from "@/components/Topbar/TopBar.vue";
+import TypingArea from "@/components/TypingArea/TypingArea.vue";
+</script>
+
+<template>
+  <section>
+    <TopBar />
+    <TypingArea />
+  </section>
+  <p class="message">Sorry, this app is not available for mobile devices.</p>
+</template>
+
+<style scoped>
+section {
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+}
+
+.message {
+  font-size: 2rem;
+  text-align: center;
+  display: none;
+  padding-inline: 1rem;
+}
+
+@media (max-width: 1024px) {
+  section {
+    display: none;
+  }
+  .message {
+    display: block;
+  }
+}
+</style>
