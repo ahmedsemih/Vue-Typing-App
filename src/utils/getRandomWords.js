de16f04ca7ee6e3d6f3355bd3ead5299e25ed7cf
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2024-02-16 09:26:30 +0300

    fix: fix text file not found issue

--- src/utils/getRandomWords.js
+++ src/utils/getRandomWords.js
@@ -1,6 +1,8 @@
+const TEXT_FILE_URL = "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa-no-swears-medium.txt";
+
 export default async (count = 100) => {
   try {
-    const response = await fetch("./src/utils/words.txt");
+    const response = await fetch(TEXT_FILE_URL);
     const data = await response.text();
     const words = data.split("\n");
     const randomWords = [];
